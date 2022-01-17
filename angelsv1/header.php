9d3f31f1e74986d04297f274738767303bbd5d51
richasare <asarerichemond@gmail.com>
2022-01-17 21:58:35 +0100

    voici les changement
    - image devis envoyant vers le pop up
    - jai mis le code pop up dans l'accueil vu que l'on va enlever la navbar
    -ajouter le code permettant d'envoyer les infos vers un emal (non fonctionnnel)

--- angelsv1/header.php
+++ angelsv1/header.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    
+
 	<title> Angels Decor</title>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link rel="icon" type="image/png" href="images/candy.png" />
@@ -18,74 +18,26 @@
 <body>
 <script src="script/script.js"></script>
 <div  style="border-bottom: 3px solid #a88230;">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-  <a class="navbar-brand" href="index.php"> <img src="Images/logo.png" width="100" height="70" class="d-inline-block align-top" alt=""></a>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  
-    <ul class="navbar-nav mr-auto" >
-    <li class="nav-item" id='mouseTarget'>
-        <a class="nav-link" href="index.php"><font size="5">Accueil</font></a>
-      </li>
-      <li class="nav-item" id='mouseTarget2'>
-        <!--<a class="nav-link" href="index.php?uc=Devis"><font size="5">Devis</font></a> -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php"> <img src="Images/logo.png" width="100" height="70" class="d-inline-block align-top" alt=""></a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-
-        <a class="nav-link" href="index.php?uc=Devis" data-toggle="modal" data-target="#myModal"><font size="5">Devis</font></a>
-              <!--ICI CE TROUVE LE POP UP -->
-                <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal" aria-hidden="true">
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel"> Devis / Contact</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                        <form>
-                          <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">Nom Complet</label>
-                            <input type="text" class="form-control" id="recipient-name">
-                          </div>
-                          <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">Adresse Mail</label>
-                            <input type="text" class="form-control" id="recipient-name">
-                          </div>
-                          <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">Telephone</label>
-                            <input type="text" class="form-control" id="recipient-name">
-                          </div>
-                          <div class="form-group">
-                            <label for="message-text" class="col-form-label">Sujet du devis</label>
-                            <textarea class="form-control" id="message-text"></textarea>
-                          </div>
-                        </form>
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                        <button type="button" class="btn btn-warning">Envoyer</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <!--ICI SE FINIT LE POP UP-->
-
-
-
-
-
-
-
-      </li>
-      </ul>
-
-      </div>     
-</div>
-</nav>
+        <ul class="navbar-nav mr-auto" >
+          <li class="nav-item" id='mouseTarget'>
+            <a class="nav-link" href="index.php"><font size="5">Accueil</font></a>
+          </li>
+          <li class="nav-item" id='mouseTarget2'>
+            <!--<a class="nav-link" href="index.php?uc=Devis"><font size="5">Devis</font></a> -->
+            <a class="nav-link" href="index.php?uc=Devis" data-toggle="modal" data-target="#myModal"><font size="5">Devis</font></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</script> 
+</script>
--- angelsv1/vues/accueil.php
+++ angelsv1/vues/accueil.php
@@ -1,11 +1,12 @@
 
-  
+
         <div class="row" style="z-index:1;position:absolute">
-            <img src='Images/ph5.png' style="width:1367px"  >
+            <img src='Images/ph5.png' style="width:179%; max-width:179% ; height:475.583px" >
             </div >
+            <center>
             <div style="z-index:2;position:absolute">
             <cadre>
-        
+
             </div>
             <div style="z-index:3;position:absolute">
             <cadre2>
@@ -19,7 +20,8 @@
                 Basée au Pré-Saint-Gervais (93), notre champ d'action s'étend à toute la France et au-delà.</font>
             </cadre2>
         </div>
-          
+      </center>
+
 </div>
 <br>
 <br>
@@ -52,15 +54,96 @@
     <p style="text-align: center"><font size="5" color="black" >Notre systeme de Devis </font></p>
     <p style="text-align: left"><font size="4" color="black" ><strong>La rapidité est l'une de nos forces.
     <br>
-    Chez Angels Decor les devis seront effectués dans les prochaines 24H après votre demande. 
+    Chez Angels Decor les devis seront effectués dans les prochaines 24H après votre demande.
     <br>
      Comptez sur nous pour ne pas vous faire perdre votre temps!</font></p>
 </strong>
     </div>
-    
+
 
     <div id='de'class="col"style="background-color: #ffffff;opacity: 0;border-radius: 10px;border:3px solid #a88230">
-    <img src="Images/devis.png" id="dei" style="width:200px;height:200px;opacity:1;margin-left: 180px">
+    <a class="nav-link" href="index.php?uc=Devis" data-toggle="modal" data-target="#myModal"><img src="Images/devis.png" id="dei" style="width:200px;height:200px;opacity:1;margin-left: 180px"></a>
+
+    <!--ICI CE TROUVE LE POP UP -->
+      <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel"> Devis / Contact</h5>
+
+                <?php
+                if(isset($_POST['mailform']))
+                {
+
+
+                  if (!empty($_POST['name']) AND !empty($_POST['mail']) AND !empty($_POST['telephone']) AND !empty($_POST['sujet']) )
+                  {
+                    $header="MIME-Version: 1.0\r\n";
+                    $header.='From:"AngelsDecor.fr"<support@angelsdecor.fr>'."\n";
+                    $header.='Content-Type:text/html; charset="uft-8"'."\n";
+                    $header.='Content-Transfer-Encoding: 8bit';
+
+                    $message='
+                    <html>
+                      <body>
+                        <div align="center">
+                          <img src="http://www.primfx.com/mailing/banniere.png"/>
+                          <br />
+                          <u> Nom de l\'expéditeur: </u>'.$_POST['name'].'<br />
+                          <u> Mail de l\'expéditeur: </u>'.$_POST['mail'].'<br />
+                          <u> Téléphone de l\'expéditeur: </u>'.$_POST['telephone'].'<br />
+                          <br />
+                          '.$_POST['sujet'].'
+                          <img src="http://www.primfx.com/mailing/separation.png"/>
+                        </div>
+                      </body>
+                    </html>
+                    ';
+
+                    mail("bob.angelsdecor@gmail.com", "Contact - Monsite.com", $message, $header);
+                  }
+                }
+                  else {
+                    $msg = "Tout les champs doivent étre completés ok? !";
+                  }
+
+
+                  ?>
+
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form method="POST" action="">
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Nom Complet</label>
+                  <input type="text" class="form-control" name="nom" placeholder="Votre nom/prenom" id="recipient-name">
+                </div>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Adresse Mail</label>
+                  <input type="text" class="form-control" name="mail" placeholder="Votre email" id="recipient-name">
+                </div>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Telephone</label>
+                  <input type="text" class="form-control" name="telephone" placeholder="Votre numero de téléphone" id="recipient-name">
+                </div>
+                <div class="form-group">
+                  <label for="message-text" class="col-form-label">Sujet du devis</label>
+                  <textarea class="form-control" name="sujet" id="message-text"></textarea>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <!--<button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>-->
+              <input type="button" name="mailform" class="btn btn-warning" value="Envoi">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--ICI SE FINIT LE POP UP-->
+
+
     <p style="text-align: center"><font id="fo"size="4" color="#a88230" >FAITES VOTRE DEVIS</font></p>
     </div>
 
@@ -73,7 +156,7 @@
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.3904656277805!2d2.3985250154263373!3d48.88889530651272!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66db1a0f0a3a9%3A0xadce8504ae2bac72!2s11%20Rue%20Jean-Baptiste%20Cl%C3%A9ment%2C%2093500%20Le%20Pr%C3%A9-Saint-Gervais!5e0!3m2!1sfr!2sfr!4v1642023252971!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
     <div class="col">
-    
+
     </div>
 
 
@@ -85,11 +168,11 @@
 <temp id="temp2" t2="0">
 <temp id="temp3" t3="0">
 <script>
-     
-    
+
+
     var time;
-    
+
         time = window.setInterval(function() {charge()}, 100);
 
-   
+
 </script>
