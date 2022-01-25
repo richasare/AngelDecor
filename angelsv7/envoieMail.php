c15db34a31855b8262a3a7114f2322a1f9e1b524
kevcachan2 <bzukaille@gmail.com>
2022-01-25 23:31:00 +0100

    v7

---
+++ angelsv7/envoieMail.php
@@ -0,0 +1,66 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create de l'instance
+$envoi_mail = new PHPMailer;
+
+$envoi_mail->Host = 'smtp.gmail.com';
+$envoi_mail->Port = 587; // 465;
+$envoi_mail->SMTPSecure = 'tls';//
+
+$envoi_mail->isSMTP();
+$envoi_mail->SMTPAuth  = true;
+
+//Mot de Passe et adresse e-mail (Gmail)
+$envoi_mail->Username = 'test.angelsdecor@gmail.com';
+$envoi_mail->Password   = 'azerty123?';
+
+$envoi_mail->SMTPDebug = 0;
+//$mail->debugoutput = 'html';
+
+$mailExpediteur = trim(filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL));
+$objet = trim(filter_input(INPUT_POST, "objet", FILTER_SANITIZE_STRING, FILTER_SANITIZE_ENCODED));
+$message = trim(filter_input(INPUT_POST, "message", FILTER_SANITIZE_STRING, FILTER_SANITIZE_ENCODED));
+
+// Expéditeur
+$mailContact = "test.angelsdecor@gmail.com";
+$envoi_mail->setFrom($mailContact);
+
+// Destinataire
+$mailDestinataire = "test.angelsdecor@gmail.com";
+$envoi_mail->addAddress($mailDestinataire);
+
+// Contenu du Mail
+$envoi_mail->Subject = $nom.$objet;
+$envoi_mail->isHTML(true);
+
+//$envoi_mail->AddEmbeddedImage('images/icon_alerte.png', 'logo_alerte');
+$body = "<h4> Monsieur, Madame :".$nom." <br> DEMANDE DE DEVIS:  ".$sujet." <br> A RECONTACTER  AU:  ".$telephone. " OU PAR EMAIL AU : ".$mail. " </h4> ";
+
+
+
+$envoi_mail->Body = $body;
+$envoi_mail->AltBody = strip_tags($body);
+
+/*
+//Pièces Jointes
+$envoi_mail->addAttachment('/images/image.jpg', 'photo.jpg');
+*/
+
+// Test (Vérification Envoi du Mail)
+if ($envoi_mail->send()) {
+    echo "Message Envoyé";
+} else {
+
+    echo "Message Non Envoyé";
+    echo $envoi_mail->ErrorInfo;
+}
+
+?>
---
+++ angelsv7/vues/accueil2.php
@@ -0,0 +1,167 @@
+<script>
+ RedirectionJavascript();
+</script>
+<script>bois();</script>
+
+
+
+<div class="container"style="padding-left:0;margin-left:0;" >
+<div style="z-index:2;position:absolute">
+    <cadre id="cd0"style="width:100vw;height:460px">
+
+</div>
+ <div style="z-index:4;position:absolute">
+ <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+    <cadre2 id="cd10" style="width:100vw;text-align:center" >
+    <button type="button" onclick="scroll1()">Devis/Galerie</button>
+    <button type="button" onclick="scroll2()">Avis</button>
+    <button type="button"onclick="scroll3()">Contact</button>
+        
+        
+    </cadre2>
+ </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="container"style="padding-left:0;margin-left:0;" >
+<div style="z-index:2;position:absolute">
+    <cadre id="cd0"style="width:100vw;height:460px;background-color:#f0ab3a;opacity:1">
+
+</div>
+<div style="z-index:3;position:absolute">
+<img src="Images/glogo2.png" class="card-img-top" style="width:60%;height:60%">
+
+</div>
+ <div style="z-index:4;position:absolute">
+ <br>
+        <br>
+        <br>
+    <cadre2 id="cd10" style="width:100vw;text-align:center" >
+
+    <a href="https://www.google.com/maps/uv?pb=!1s0x47e663ac32461f05%3A0xe7b79ac4ba26cb00!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOK57PxhaE4XVXNEoa_AF0ASAtDrxDQSzjcNOiD%3Dw213-h160-k-no!5sangels%20decor%20-%20Recherche%20Google!15sCgIgAQ&imagekey=!1e10!2sAF1QipOK57PxhaE4XVXNEoa_AF0ASAtDrxDQSzjcNOiD&hl=fr&sa=X&ved=2ahUKEwiYg7HRscL1AhVKzoUKHSrTBDgQoip6BAghEAM">
+    <div class="card" style="width: 18rem;">
+  <img src="Images/galerie3.png" class="card-img-top" alt="..."style="width:50%;height:50%">
+  <div class="card-body">
+    <h5 class="card-title">Galerie</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    
+  </div>
+</div>
+</a>
+
+<div class="card" style="width: 18rem;">
+  <img src="Images/devis2.png" class="card-img-top" alt="..."style="width:50%;height:50%">
+  <div class="card-body">
+    <h5 class="card-title">Devis sous 24H</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    
+  </div>
+</div>
+  
+        
+        
+    </cadre2>
+
+
+
+
+</div>    
+
+</div>   
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="container"style="padding-left:0;margin-left:0;" >
+<div style="z-index:2;position:absolute">
+    <cadre id="cd0"style="width:100vw;height:460px;background-color:#a6a6a6;opacity:1">
+
+</div>
+<div style="z-index:3;position:absolute;text-align:end ;width:100vw">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<img src="Images/glogo.png" class="card-img-top" style="width:13%;height:13%">
+
+</div>
+ <div style="z-index:4;position:absolute">
+ <br>
+        <br>
+        <br>
+        <br>
+
+    <cadre2 id="cd10" style="width:50vw;text-align:center" >
+          <!--LES AVIS GOOGLE-->
+          <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+      <div class="elfsight-app-daa2c6f8-cbab-4935-8fd5-3b8b5adca3e4"></div>
+      <!--FIN les AVIS GOOGLE-->
+        
+        
+    </cadre2>
+    
+ </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
