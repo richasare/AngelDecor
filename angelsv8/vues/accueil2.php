2bf86d301c3d74f31bf7a6420126c82fb6ec5889
kevcachan2 <bzukaille@gmail.com>
2022-01-27 18:04:15 +0100

    final

---
+++ angelsv8/vues/accueil2.php
@@ -0,0 +1,177 @@
+<script>
+ RedirectionJavascript();
+</script>
+<script>bois();</script>
+
+
+
+<div class="container"style="padding-left:0;margin-left:0;" >
+<div style="z-index:2;position:absolute">
+    <cadre id="cd0"style="width:100vw;height:800px">
+
+</div>
+<div style="z-index:3;position:fixed">
+<img src="Images/logo4.png" class="card-img-top" style="width:60%;height:60%;margin-left:30vw;margin-top:12vh;opacity:0.85">
+</div>
+ <div style="z-index:4;position:absolute">
+ <br>
+        <br>
+ 
+    <cadre2 id="cd10" style="width:100vw;text-align:center" >
+   
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="" style="background-color:black;opacity:0.75; width:72vw; height:225px; display:inline-block;">  
+    </div>   
+    </cadre2>
+ </div>
+ <div style="z-index:5;position:absolute; text-align:center; " >
+ <cadre2 id="cd10" style="width:100vw;text-align:center" >
+ <button type="button" onclick="scroll1()">Devis/Galerie</button>
+    <button type="button" onclick="scroll2()">Avis</button>
+    <button type="button"onclick="scroll3()">Contact</button>
+    <br>
+   <br><br><br><br><br>
+   <h4 style="opacity:1; width:70vw; color:white;margin-left:15vw">Spécialisée dans la pose et la restauration de parquets de haute qualité, Angels Décor est une jeune société composée de professionnels expérimentés et passionnés menés par Angel Tonev. Notre exigence de produire chaque fois un travail d'exception nous vaut la satisfaction de nos clients et la reconnaissance de nos paires. Basée au Pré-Saint-Gervais (93), notre champ d'action s'étend à toute la France et au-delà.</h4>
+   </cadre2>
+  </div>
+
+
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="container"style="padding-left:0;margin-left:0;" >
+<div style="z-index:2;position:absolute">
+    <cadre id="cd0"style="width:100vw;height:460px;background-color:#6d4e1d;opacity:1">
+
+</div>
+
+ <div style="z-index:4;position:absolute">
+ <br>
+        <br>
+        <br>
+    <cadre2 id="cd10" style="width:100vw;text-align:center" >
+
+    <a href="https://www.google.com/maps/uv?pb=!1s0x47e663ac32461f05%3A0xe7b79ac4ba26cb00!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOK57PxhaE4XVXNEoa_AF0ASAtDrxDQSzjcNOiD%3Dw213-h160-k-no!5sangels%20decor%20-%20Recherche%20Google!15sCgIgAQ&imagekey=!1e10!2sAF1QipOK57PxhaE4XVXNEoa_AF0ASAtDrxDQSzjcNOiD&hl=fr&sa=X&ved=2ahUKEwiYg7HRscL1AhVKzoUKHSrTBDgQoip6BAghEAM">
+    <div class="card" style="width: 28rem;">
+  <img src="Images/galerie3.png" class="card-img-top" alt="..."style="width:50%;height:50%">
+  <div class="card-body">
+    <h5 class="card-title">Galerie</h5>
+    
+    
+  </div>
+</div>
+</a>
+
+<div class="card" style="width: 28rem;">
+  <img src="Images/devis2.png" class="card-img-top" alt="..."style="width:50%;height:50%">
+  <div class="card-body">
+    <h5 class="card-title">Devis(Sous 24H!!)</h5>
+    
+    
+  </div>
+</div>
+  
+        
+        
+    </cadre2>
+
+
+
+
+</div>    
+
+</div>   
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="container"style="padding-left:0;margin-left:0;" >
+<div style="z-index:2;position:absolute">
+    <cadre id="cd0"style="width:100vw;height:460px;background-color:#a6a6a6;opacity:1">
+
+</div>
+
+ <div style="z-index:4;position:absolute">
+ <br>
+        <br>
+        
+
+    <cadre2 id="cd10" style="width:50vw;text-align:center" >
+          <!--LES AVIS GOOGLE-->
+          <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+<div class="elfsight-app-daa2c6f8-cbab-4935-8fd5-3b8b5adca3e4"style="margin-left:500px;width:50vw"></div>
+      <!--FIN les AVIS GOOGLE-->
+        
+        
+    </cadre2>
+    
+ </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
