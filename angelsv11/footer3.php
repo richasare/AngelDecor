8f32f52a0ee5309a159ab96bd574fb1b97b703b9
kevcachan2 <bzukaille@gmail.com>
2022-02-07 08:26:20 +0100

    final de final de final

---
+++ angelsv11/footer3.php
@@ -0,0 +1,83 @@
+<footer class="bg-light text-center text-lg-start"style="position:absolute;z-index:1;width:100vw;margin-left:0vw;margin-right:0vw;height:150px;margin-top:1680px;padding-top:50px;">
+  <!-- Grid container -->
+  <div>
+    <!--Grid row-->
+    <div class="row"style="height:100%">
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <!-- <h5 class="text-uppercase">Links</h5> -->
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <!-- <a href="#!" class="text-dark">Link 1</a> -->
+            <img src="Images\logo5.png" width="100px" alt="" style="text-align:center; display:auto; margin-left:25%; width:41%;" >
+
+          </li>
+
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+
+
+        <ul class="list-unstyled">
+          <li>
+
+            <br>
+            <br>
+
+            <p style="color:grey;font-size: 0.8em;margin-bottom: 10px;"><i class="fas fa-home me-3"></i> 29 RTE DE VERSAILLES 78560 LE PORT-MARLY</p>
+          </li>
+
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <br>
+            <br>
+
+            <a class="fas fa-envelope me-3" href="mailto:info-angelsdecor.com" style="color:grey;font-size: 1.2em;"> info@angels-decor.com</a>
+
+          </li>
+
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+
+
+        <ul class="list-unstyled">
+          <li>
+            <br>
+              <br>
+
+            <a class="fas fa-phone me-3" href="tel:+33659739475" style="color:grey;font-size: 1.2em;">  06 59 73 94 75</a>
+          </li>
+
+        </ul>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <!-- <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+  © 2022 Angels Decor.Tout droits réservé.
+    <!-- <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a> -->
+  <!-- </div> -->
+  <!-- Copyright -->
+</footer>
+</body>
+</html>
---
+++ angelsv11/vues/accueil3.php
@@ -0,0 +1,70 @@
+<script>
+ RedirectionJavascript();
+</script>
+<script>bois();</script>
+
+
+
+<div style="z-index:3;position:fixed">
+  <img src="Images/logo7.png" class="card-img-top" style="width:60%;height:60%;margin-left:20vw;margin-top:43px;opacity:0.85">
+</div>
+<div style="z-index:5;position:fixed">
+  <img src="Images/logo6.png" class="card-img-top" style="width:60%;height:60%;margin-left:20vw;opacity:0.85; margin-top:30px;">
+</div>
+<!-- Bouton devis/galerie -->
+<div style="margin-top:25px;position:absolute;z-index:6">
+  <a href="https://www.google.com/maps/uv?pb=!1s0x47e663ac32461f05%3A0xe7b79ac4ba26cb00!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOK57PxhaE4XVXNEoa_AF0ASAtDrxDQSzjcNOiD%3Dw213-h160-k-no!5sangels%20decor%20-%20Recherche%20Google!15sCgIgAQ&imagekey=!1e10!2sAF1QipOK57PxhaE4XVXNEoa_AF0ASAtDrxDQSzjcNOiD&hl=fr&sa=X&ved=2ahUKEwiYg7HRscL1AhVKzoUKHSrTBDgQoip6BAghEAM">
+    <div class="card" id="cr1" style="width: 22rem;text-align:center;margin-left:10vw;">
+      <img src="Images/galerie3.png" id="gal"class="card-img-top" alt="..."style="width:40%;height:40%">
+      <div class="card-body">
+        <h1 class="card-title" style="font:bold large">Galerie <br><br></h1>
+      </div>
+    </div>
+  </a>
+
+  <a data-toggle="modal" data-target="#myModal" href="#">
+    <div class="card" id="cr2" style="width: 22rem;text-align:center;margin-left:25vw ">
+      <img src="Images/devis2.png" id="dei"class="card-img-top" alt="..."style="width:40%;height:40%;">
+      <div class="card-body">
+        <h1 class="card-title" style="font:bold large"> Devis </h1>
+        <h3 class="card-title" style="font:bold large"> en moins de 24H </h3>
+      </div>
+    </div>
+  </a>
+</div>
+<!-- Fin bouton devis/galerie -->
+
+<!-- Debut bouton appel -->
+<div style="margin-top:500px;position:absolute;z-index:6">
+  <a  href="tel:+33659739475" >
+    <button type="button"style="width:30vw;margin-left:35vw"><i class="fas fa-phone me-3"  style="font-size: 1.2em;">  06 59 73 94 75</i></button>
+  </a>
+</div>
+<!-- Fin bouton appel -->
+
+<!-- Debut du cadre gris -->
+<div style="margin-top:700px;position:absolute;z-index:1;height:700px;width:100vw;background-color:grey; ">
+</div>
+    <cadre2 id="cd10" style="width:70vw;text-align:center; height:100px;">
+        <!--LES AVIS GOOGLE-->
+        <div style='position:absolute;z-index:6;margin-top:900px;text-align:center;width:70vw;margin-left:20vw;'>
+        <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+        <div class="elfsight-app-daa2c6f8-cbab-4935-8fd5-3b8b5adca3e4" style=''></div>
+</div>
+        <!--FIN les AVIS GOOGLE-->
+    </cadre2>
+  
+
+
+<!-- Fin du cadre gris -->
+
+<!-- Debut du cadre: A propos de nous -->
+<div style="margin-top:1200px;position:absolute;z-index:4;height:500px;width:100vw;background-color:#FFFFFF;"></div>
+<div style='position:absolute;z-index:6;margin-top:1300px;text-align:center;width:70vw;margin-left:7vw;'>
+  <h1 style="text-align:center; text-shadow:2px 3px 1px black;color:white; margin-top:60px;">A propos de nous</h1>
+  <div style='position:absolute;z-index:6;margin-top:30px;text-align:center;width:70vw;margin-left:0vw;'>
+     <h3 style="opacity:1; width:70vw; color:white;margin-left:10vw;text-shadow:2px 3px 1px black; margin-top:0px;">Spécialisée dans la pose et la restauration de parquets de haute qualité, Angels Décor est une jeune société composée de professionnels expérimentés et passionnés menés par Angel Tonev. Notre exigence de produire chaque fois un travail d'exception nous vaut la satisfaction de nos clients et la reconnaissance de nos paires. Basée au Pré-Saint-Gervais (93), notre champ d'action s'étend à toute la France et au-delà.</h3>
+     </div>
+</div>
+<!-- Fin du cadre: A propos de nous -->
+<script>navbar();</script>
